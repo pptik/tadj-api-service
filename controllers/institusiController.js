e5531c0de215f83d6803949948b3fcad9f462123
emilhamep <emilhamep@gmail.com>
2017-08-08 12:06:08 +0700

    api menampilkan daftar mhs berdasarkan institusi

--- controllers/institusiController.js
+++ controllers/institusiController.js
@@ -6,19 +6,62 @@ var Pengguna = require('../models/userModel');
 //Import library
 var async = require('async');
 var moment = require('moment');
+var restClient = require('node-rest-client').Client;
+var rClient = new restClient();
+var base_api_url = 'http://localhost:3001';
 
 
 exports.daftar_mahasiswa = function(req,res) {
 
-  Pengguna.find({akademik:{peran:4}})
-   .exec(function (err, results) {
+  //Inisial validasi
+  req.checkBody('access_token', 'Akses token tidak boleh kosong').notEmpty();
+  req.checkBody('idUniversitas', 'Id universitas tidak boleh kosong').notEmpty();
 
-     if (err) {
-       return res.json({success: false, data: err})
-     }else{
-       return res.json({success: true, data: results})
-     }
+  //Dibersihkan dari Special Character
+  req.sanitize('access_token').escape();
+  req.sanitize('idUniversitas').escape();
 
-   });
+  req.sanitize('access_token').trim();
+  req.sanitize('idUniversitas').trim();
 
+  //Menjalankan validasi
+  var errors = req.validationErrors();
+
+  if(errors){//Terjadinya kesalahan
+      return res.json({success: false, data: errors})
+  }else{
+
+    args = {
+      	data: {
+          access_token: req.body.access_token},
+      	headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    };
+
+    rClient.post(base_api_url+'/cek_session', args, function (data, response) {
+        //Kelas.find({ _id:idKelas , pengajar: {$elemMatch:{mapel:idMapel,guru:idGuru}} }).exec(function (err, results) {
+
+        if(data.success == true){//session berlaku
+          var idUniversitas = req.body.idUniversitas
+          console.log('id univ:'+idUniversitas)
+          Pengguna.find({$and:[
+          {'akademik.peran':4},
+          {'akademik.universitas_prodi.universitas':idUniversitas}
+
+        ]})
+           .exec(function (err, results) {
+
+             if (err) {
+               return res.json({success: false, data: err})
+             }else{
+               return res.json({success: true, data: results})
+             }
+
+           });
+
+        }else{//session tidak berlaku
+          return res.json({success: false, data: {message:'Token tidak berlaku'}})
+        }
+
+    })
+  }
 }
